78e8540131177712a4cae97e8d6534f75c5a77cd
TajmulHossainDev <tajmulhossain6991@gmail.com>
2026-03-07 22:44:07 +0300

    login page functionality done

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello")
+const username = document.getElementById("usernameInput");
+const password = document.getElementById("passwordInput");
+const signupBtn = document.getElementById("signupBtn");
+
+signupBtn.addEventListener("click", function(){
+    const userId = username.value;
+    const userPass = password.value;
+
+    if(userId === "admin" && userPass === "admin123"){
+        window.location.href = "index.html";
+    }
+    else{
+        alert("invalid username or password");
+    }
+})
